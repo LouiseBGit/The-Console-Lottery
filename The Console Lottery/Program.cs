66d1d999ccad8d7e5a997eb67f60b5ddb4a003f0
LouiseBGit <lowalis91@gmail.com>
2025-10-01 13:41:08 +0200

    Add project files.

---
+++ The Console Lottery/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace The_Console_Lottery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
