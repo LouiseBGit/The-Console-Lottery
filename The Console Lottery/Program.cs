476af1b63656fa437ea0d4125eee7ccc583b8212
DanielNilsson-GIT <daniel.gronberg92@gmail.com>
2025-10-01 14:32:13 +0200

    added random function

--- The Console Lottery/Program.cs
+++ The Console Lottery/Program.cs
@@ -2,10 +2,27 @@
 {
     internal class Program
     {
+        static int[] winningnumbers = new int [3];
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.Write("test");
+            
+            
+        }
+
+        static void NumberGenerator()
+        {
+            Random winningNumber = new Random();
+
+            for (int spins = 0; spins <= 2; spins++)
+            {
+                int resultNumber = winningNumber.Next(1, 51);
+                winningnumbers[spins] = resultNumber;
+                
+                Console.WriteLine(resultNumber);
+
+            }
+
+
         }
     }
 }
